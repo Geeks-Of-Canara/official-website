87f191b3eea26720beeef3a59ee000c49464c77d
Udith Bekal <udithbkl100@gmail.com>
2022-12-10 01:47:26 +0530

    added baloons and modal

--- script.js
+++ script.js
@@ -73,3 +73,24 @@ btn4.addEventListener("click", (e) => {
     clearInterval(interval);
   }
 });
+
+
+
+// modal
+
+
+var mbtn=document.querySelector('.view-more');
+var modal = document.querySelector('.modal');
+var close = document.querySelector('.close');
+
+
+mbtn.addEventListener('click',()=>
+{
+    modal.style.display="flex";
+})
+
+close.addEventListener('click',()=>
+{
+  modal.style.display="none";
+})
+
