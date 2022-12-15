7b22680e9df9b069d7f2351f30005101ed0d4bcd
Sandesh H D <sandeshhd16@gmail.com>
2022-12-15 22:19:13 +0530

    URL change

--- events.js
+++ events.js
@@ -10,7 +10,7 @@ function load(url,method,callback) {
     xhttp.send();
   }
   
-load("http://152.67.1.27:3000/allEvents","get",(value)=>{
+load("https://sandesh-backend.vercel.app/","get",(value)=>{
 list = JSON.parse(value);
 var cardContainer = document.querySelector(".card-container");
 var cards = "";
