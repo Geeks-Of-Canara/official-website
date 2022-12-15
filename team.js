1b157bb61daa99fc4a267d959219087862cf1701
Sandesh H D <sandeshhd16@gmail.com>
2022-12-15 10:14:50 +0530

    Tagline update

--- team.js
+++ team.js
@@ -1,13 +1,18 @@
 var student = document.querySelector('.inner_text');
 var faculty = document.getElementById('Faculty');
-
+var student_data = document.getElementById('container-1')
+var faculty_data = document.getElementById('container-2')
 student.addEventListener("click", function() {
-    student.classList.toggle("active");
-    faculty.classList.toggle("active");
+    student.classList.add("active");
+    student_data.classList.add('active');
+    faculty_data.classList.remove('active');
+    faculty.classList.remove("active");
 });
 
 faculty.addEventListener("click", function() {
-    faculty.classList.toggle("active");
+    faculty.classList.add("active");
+    faculty_data.classList.add('active');
+    student_data.classList.remove('active');
     student.classList.remove("active");
 
 });
